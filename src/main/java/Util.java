e10c3ecfa2f009a356f0f7718f519e6a08f96616
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-04-14 10:52:27 +0400

    Добавил форматы стрелок и конфигурацию через system:env

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -8,14 +8,13 @@ public class Util {
     public static String pathBuilder(String... path) {
         StringBuilder sb = new StringBuilder();
         for (String s : path) {
-//            sb.append(s).append("/");
             sb.append(s).append(File.separator);
         }
         return new String(sb);
     }
 
     public static void createTxtFile(List<String> list) {
-        File fileTxt = new File(Util.pathBuilder("target") + "marmaid_map.txt");
+        File fileTxt = new File("autotest_map.txt");
         try (FileWriter writer = new FileWriter(fileTxt, false)) {
             for (String tag : list) {
                 writer.write(tag + "\n");
